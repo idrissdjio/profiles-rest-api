ea247e4e17c96108b0e6cf36687569e08cbe9b0c
Idriss <djiofackidriss@gmail.com>
2021-03-29 23:00:09 +0800

    Added profiles viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,8 +2,11 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
 
 from profiles_api import serializers
+from profiles_api import models
+from profiles_api import permissions
 
 
 class HelloApiView(APIView):
@@ -57,7 +60,7 @@ class HelloViewSet(viewsets.ViewSet):
         a_viewset = [
         'Uses actions (list, create, retrieve, update, partial_update)',
         'AUtomatically maps to URLs using Routers',
-        'Provides mre functionality with less code',
+        'Provides more functionality with less code',
         ]
 
         return Response({"message": 'Hello', "a_viewset": a_viewset})
@@ -91,3 +94,10 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle destroying an object by its ID"""
         return Response({'http method': 'DELETE'})
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (permissions.UpdateOwnProfile,)
